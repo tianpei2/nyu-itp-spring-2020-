952497c0b0c87b140ee1535ad20a906ad6af765a
zz2061 <zz2061@nyu.edu>
2020-03-10 22:21:06 -0400

    Add Refresh Bar
    Refresh bar added on App bar.

--- web/src/Nav.js
+++ web/src/Nav.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from '@material-ui/core/AppBar';
@@ -12,6 +13,33 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
 import {UserContext} from './User';
 
+=======
+import {
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+  Button
+} from "@material-ui/core";
+import { PlaylistAdd } from "@material-ui/icons";
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import {
+  usePopupState,
+  bindMenu,
+  bindTrigger
+} from "material-ui-popup-state/hooks";
+import React from "react";
+
+import { UserContext, logOut } from "./User";
+import CachedIcon from '@material-ui/icons/Cached';
+>>>>>>> Stashed changes
 
 const styles = theme => ({
   menuButton: {
@@ -29,6 +57,7 @@ class Nav extends React.Component {
     };
   }
 
+<<<<<<< Updated upstream
   render() {
     const { classes } = this.props;
     const { anchorEl } = this.state;
@@ -52,15 +81,49 @@ class Nav extends React.Component {
 
             <Box flexGrow={1}>
               <Typography variant="h6" className={classes.title} color="inherit" noWrap>
+=======
+          <Box flexGrow={2} ml={2}>
+            <Typography variant="h6" color="inherit" noWrap>
+              <Link
+                component={RouterLink}
+                to="/"
+                underline="none"
+                color="inherit"
+              >
+>>>>>>> Stashed changes
                 Marsbot Audio
               </Typography>
             </Box>
 
+<<<<<<< Updated upstream
             {user && (
               <div>
                 <Avatar
                   alt={user.name}
                   src={user.picture}
+=======
+          <Box flexGrow={50}>
+          <Button
+            size="small"
+            target="http://marsbot-audio.herokuapp.com" href="index"
+            variant="contained"
+            startIcon={<CachedIcon/>}
+            >
+          </Button>
+          </Box>
+
+          {user && (
+            <>
+              <Link component={RouterLink} to="/upload" color="inherit">
+                <Tooltip title="Upload a new marsbotaudio" aria-label="upload">
+                  <IconButton color="inherit">
+                    <PlaylistAdd fontSize="large" />
+                  </IconButton>
+                </Tooltip>
+              </Link>
+              <Box ml={1}>
+                <IconButton
+>>>>>>> Stashed changes
                   aria-label="account of current user"
                   aria-controls="menu-appbar"
                   aria-haspopup="true"

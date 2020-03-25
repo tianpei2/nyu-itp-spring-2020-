c382b65c7fb01e7cb691c15e3b76ab9f21f60f76
Tian, Jin <2468952+toctan@users.noreply.github.com>
2020-03-25 14:43:14 -0400

    Revert "Audio Recording functionality"

--- src/AudioUpload.js
+++ src/AudioUpload.js
@@ -7,12 +7,9 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import React from "react";
-import Grid from '@material-ui/core/Grid';
 
 import FoursquareSuggest from "./FoursquareSuggest";
 import foursquare from "./APIClient";
-import AudioRecord from './AudioRecord'
-
 
 const useStyles = makeStyles(theme => ({
   dropzone: {
@@ -33,19 +30,14 @@ export default function AudioUpload() {
   const [files, setFiles] = React.useState([]);
   const classes = useStyles();
   const history = useHistory();
+
   const handleSubmit = event => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    if (files instanceof Blob) {
-      formData.append("ext", "mp3");
-      formData.append("file", files);
-    }
-    else {
-      if (!files.length) return alert("No audio file selected.");
-      const ext = files[0].name.split(".").pop();
-      formData.append("ext", ext);
-      formData.append("file", files[0]);
-    }
+    if (!files.length) return alert("No audio file selected.");
+    const ext = files[0].name.split(".").pop();
+    formData.append("ext", ext);
+    formData.append("file", files[0]);
 
     const action = formData.get("action");
     if (action) {
@@ -66,13 +58,15 @@ export default function AudioUpload() {
   };
 
   return (
-    <Container component="main" maxWidth="sm">
+    <Container component="main" maxWidth="xs">
       <Box mt={8}>
         <Typography component="h1" variant="h5">
           Upload an audio
         </Typography>
+
         <form onSubmit={handleSubmit}>
           <FoursquareSuggest />
+
           <RadioGroup name="action" row>
             <FormControlLabel
               value="setName"
@@ -85,25 +79,18 @@ export default function AudioUpload() {
               label="Set jingle"
             />
           </RadioGroup>
-          <Grid container spacing = {3}>
-            <Grid item xs={6}>
-              <FormControl fullWidth margin="normal">
-                <DropzoneArea
-                  dropzoneText={"Drag 'n' drop, or click to select"}
-                  onChange={setFiles}
-                  acceptedFiles={["audio/*"]}
-                  dropzoneClass={classes.dropzone}
-                  useChipsForPreview
-                  filesLimit={1}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={6}>
-              <AudioRecord
-                setFiles={setFiles}
-              />
-            </Grid>
-          </Grid>
+
+          <FormControl fullWidth margin="normal">
+            <DropzoneArea
+              dropzoneText={"Drag 'n' drop, or click to select"}
+              onChange={setFiles}
+              acceptedFiles={["audio/*"]}
+              dropzoneClass={classes.dropzone}
+              useChipsForPreview
+              filesLimit={1}
+            />
+          </FormControl>
+
           <FormControl fullWidth margin="normal">
             <Button
               type="submit"

dd537da669dc17d1f6a48c50286f33da963424c2
tianjin <toctan@users.noreply.github.com>
2020-04-16 10:39:28 -0400

    Fix /channel/:id/delete

--- src/ChannelDelete.js
+++ src/ChannelDelete.js
@@ -1,31 +1,33 @@
-import React from "react";
 import { useHistory, useLocation, useParams } from "react-router-dom";
+import React from "react";
+
 import qs from "qs";
 
+import User from './User';
 import foursquare from "./APIClient";
 
 export default (props) => {
   let { id } = useParams();
   const history = useHistory();
   const location = useLocation();
+  const { user } = React.useContext(User.Context);
 
   const handleCancel = () => {
     const background = location.state && location.state.background;
-    if (background) history.goBack();
-    else history.push("/");
+    history.push(background || "/");
   };
 
   React.useEffect(() => {
     if (!window.confirm("Are you sure you want to delete this channel?"))
       return handleCancel();
-    return foursquare
+    foursquare
       .post(
         "demo/marsbot/audio/channels/delete",
         qs.stringify({
           id,
         })
       )
-      .then((resp) => history.replace("/channels"));
+      .then((resp) => history.replace(`/user/${user.id}/channels`));
   });
 
   return null;
